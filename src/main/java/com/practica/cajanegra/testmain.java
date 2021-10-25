878c47fc500745e308a3da0bba60b9e66f6d2b2f
Javier Cano <j.cdelatorre@alumnos.upm.es>
2021-10-25 17:44:38 +0200

    Caso inicial de prueba

---
+++ src/main/java/com/practica/cajanegra/testmain.java
@@ -0,0 +1,9 @@
+package com.practica.cajanegra;
+
+import com.cajanegra.SingleLinkedListImpl;
+
+public class testmain {
+    public static void main(String [] args){
+        SingleLinkedListImpl<Integer> potato;
+    }
+}
