a0874cefb902841e423c78c53396bafb30078970
Javier Cano <j.cdelatorre@alumnos.upm.es>
2021-11-08 18:45:04 +0100

    Inicio de los test de esta clase

--- src/test/java/com/practica/cajanegra/AddAtPostTest.java
+++ src/test/java/com/practica/cajanegra/AddAtPostTest.java
@@ -1,5 +1,30 @@
 package com.practica.cajanegra;
-
+import com.cajanegra.SingleLinkedListImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 public class AddAtPostTest {
+    static SingleLinkedListImpl<String> list;
+
+    @BeforeEach
+    void setup() {
+        list = new SingleLinkedListImpl<>("A", "B", "C", "D", "E", "F", "G");
+    }
+
+    @Test
+     void addAtPosIndex0(){
+        Assertions.assertThrows(IllegalArgumentException.class,() -> list.addAtPos("Z",0));
+    }
+    @Test
+    void addAtPosIndex1(){
+        list.addAtPos("Z",1);
+        Assertions.assertEquals("Z",list.getAtPos(1));
+    }
+    @Test
+    void addAtPosIndexN(){
+        list.addAtPos("Z", list.size());
+        Assertions.assertEquals("Z",list.getAtPos(list.size()));
+    }
 
 }
