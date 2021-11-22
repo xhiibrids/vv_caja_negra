0a8d8879c10a0774870b0e3412bcd92c4fc24a92
fran <franciscoandres.ferreyra@alumnos.upm.es>
2021-11-22 14:12:37 +0100

    InitIndexandaddfirst

--- src/test/java/com/practica/cajanegra/AddFirstTest.java
+++ src/test/java/com/practica/cajanegra/AddFirstTest.java
@@ -1,4 +1,25 @@
 package com.practica.cajanegra;
 
+import com.cajanegra.SingleLinkedListImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
 public class AddFirstTest {
+    static SingleLinkedListImpl<String> list;
+
+    @BeforeEach
+    void init() {
+        list = new SingleLinkedListImpl<>();
+    }
+
+    @Test
+    void addFirstValid(){
+
+    }
+
+
 }
--- src/test/java/com/practica/cajanegra/IndexOfTest.java
+++ src/test/java/com/practica/cajanegra/IndexOfTest.java
@@ -1,4 +1,19 @@
 package com.practica.cajanegra;
 
+import com.cajanegra.SingleLinkedListImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class IndexOfTest {
+    static SingleLinkedListImpl<String> list;
+
+    @BeforeEach
+    void init() {
+        list = new SingleLinkedListImpl<>();
+    }
+
+    @Test
+    void addFirstValid(){
+
+    }
 }
