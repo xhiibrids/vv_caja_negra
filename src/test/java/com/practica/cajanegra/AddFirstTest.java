39eb3efb33aced2d710cad7ec30a9437bb44e1f3
fran <franciscoandres.ferreyra@alumnos.upm.es>
2021-11-23 13:38:26 +0100

    AddFirst cambio

--- src/test/java/com/practica/cajanegra/AddFirstTest.java
+++ src/test/java/com/practica/cajanegra/AddFirstTest.java
@@ -1,10 +1,7 @@
 package com.practica.cajanegra;
 
 import com.cajanegra.SingleLinkedListImpl;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -16,7 +13,7 @@ public class AddFirstTest {
         list = new SingleLinkedListImpl<>("A", "B", "C", "D", "E", "F", "G");
     }
 
-    @Test
+
     @DisplayName("Test añadir válidos")
     @ParameterizedTest
     @MethodSource(value = "validData")
@@ -34,7 +31,15 @@ public class AddFirstTest {
                 {"Z", "Z"}
         };
     }
+    @DisplayName("Test vacío añadir válidos")
+    @ParameterizedTest
+    @MethodSource(value = "validData")
+    void addFirstEmptyValid(String element,String result) {
+        list = new SingleLinkedListImpl<>();
+        list.addFirst(element);
+        Assertions.assertThrows(java.lang.IllegalArgumentException.class, () -> list.getAtPos(1));
 
+    }
 
     @DisplayName("Test valores inválidos")
     @ParameterizedTest
@@ -45,29 +50,25 @@ public class AddFirstTest {
 
     }
 
-    @DisplayName("Test valor añadido valido")
-    @ParameterizedTest
-    @MethodSource(value = "validData")
-    void addFirstVaciavalid(String element, String result){
-        list = new SingleLinkedListImpl<>();
-        list.addFirst(element);
-        Assertions.assertEquals(result,list.getAtPos(1));
-    }
-
-    @DisplayName("Test valor añadido invalido")
+    @DisplayName("Test vacío valores inválidos")
     @ParameterizedTest
     @MethodSource(value = "invalidData")
-    void addFirstVaciaInvalid(String element, String result){
+    void addFirstEmptyInvalid(String element, String result) {
         list = new SingleLinkedListImpl<>();
         list.addFirst(element);
-        Assertions.assertEquals(result,list.getAtPos(1));
+
+        Assertions.assertThrows(java.lang.IllegalArgumentException.class, () -> list.getAtPos(1));
     }
+
+
     static String[][] invalidData() {
         return new String[][]{
                 {"@", "@"},
                 {"[", "["}
         };
     }
+
+
 
 
 }
